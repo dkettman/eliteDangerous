50495c13a99615b814b123bca7536e0aac4b9500
Dave Kettmann <david.kettmann@gmail.com>
2020-11-10 23:16:59 -0500

    Further Modifications

--- main-watchdog.py
+++ main-watchdog.py
@@ -1,67 +1,125 @@
 import json
-from pathlib import Path
-from threading import Event, Thread
+import os
+import status_enums
 
-from watchdog.events import FileSystemEventHandler
+from pathlib import Path
+from watchdog.events import (
+    FileModifiedEvent,
+    RegexMatchingEventHandler,
+)
 from watchdog.observers import Observer
 
 
-class LogEvent(FileSystemEventHandler):
-    def __init__(self, t_event: Event):
-        self._t_event = t_event
-        super().__init__()
+class EDWatcher:
+    def __init__(self, *args, **kwargs):
+        self._pips = {"sys": "", "eng": "", "wep": ""}
+        self._status_flags = {}
+        self._legal_state = ""
+        self._cargo_cur = 0
+        self._cargo_max = 0
+        self._fuel_cur = 0
+        self._fuel_max = 0
+        self._cargo_hold = {}
 
-    def on_modified(self, event):
-        print("Caught modified event! {}".format(event))
-        self._t_event.set()
+    def set_status_flags(self, flags: dict):
+        # print(f"set_status_flags - Received: {flags}")
+        self._status_flags = flags
 
+    def set_pip(self, sys: str, pips: int):
+        # print(f"set_pip - Received: {sys}, {pips}")
+        self._pips[sys] = pips
 
-class EDLogWatcher:
-    def __init__(self, log_file: Path):
-        print("Starting watcher for: {}".format(log_file))
-        self._file_modified_event = Event()
-        self._log_handle = log_file.open()
-        # Creating the read lines handler
-        self._read_file_thread = Thread(target=self.read_lines)
-        # Way to get the thread to exit, will still need to set the event also so that
-        # it loops one more time
-        self._run = True
-        self._read_file_thread.start()
-        self._event_handler = LogEvent(self._file_modified_event)
-        self._observer = Observer()
-        # noinspection PyTypeChecker
-        self._observer.schedule(self._event_handler, log_file)
+    def get_pip(self, sys: str):
+        return status_enums.SystemPips.power_pips[self._pips[sys]]
 
-    def handle_line(self, line: str):
-        parsed = json.loads(line)
-        print("Timestamp: {} -- Event: {}".format(parsed['timestamp'], parsed['event']))
+    def set_cargo_max(self,max: int):
+        self._cargo_max = max
 
-    def read_lines(self):
-        while self._run:
-            # Wait for the event.set from LogEvent
-            self._file_modified_event.wait()
-            # Clear it immediately
-            self._file_modified_event.clear()
-            # If there aren't any new lines to read we get an empty array and nothing
-            # is run
-            for line in self._log_handle.readlines():
-                self.handle_line(line)
+    def get_cargo_max(self):
+        return self._cargo_max
 
-    def _kill_line_reader(self):
-        self._run = False
-        self._file_modified_event.set()
+    def set_cargo_cur(self,cur: int):
+        self._cargo_cur = cur
 
-log_dir = Path('c:\\Users\\chili\\Saved Games\\Frontier Developments\\Elite Dangerous')
-watchme = EDLogWatcher(log_file=log_dir / 'Journal.201109145516.01.log')
+    def get_cargo_cur(self):
+        return self._cargo_cur
 
-# watchmen = {
-#     'Cargo': EDLogWatcher(log_file=log_dir / 'Cargo.json'),
-#     'Market': EDLogWatcher(log_file=log_dir / 'Market.json'),
-#     'Modules': EDLogWatcher(log_file=log_dir / 'ModulesInfo.json'),
-#     'NavRoute': EDLogWatcher(log_file=log_dir / 'NavRoute.json'),
-#     'Outfitting': EDLogWatcher(log_file=log_dir / 'Outfitting.json'),
-#     'Shipyard': EDLogWatcher(log_file=log_dir / 'Shipyard.json'),
-#     'Status': EDLogWatcher(log_file=log_dir / 'Status.json')
-# }
+    def reset_cargo(self):
+        self._cargo_hold = {}
+
+    def update_cargo(self, cargo: dict):
+        self._cargo_hold[cargo['Name']] = cargo['Count']
+
+    def get_cargo(self):
+        return self._cargo_hold
 
 
+class EDLogWatcher(RegexMatchingEventHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._func_dict = {
+            "EVENT_TYPE_CREATED": self.on_any_event,
+            "EVENT_TYPE_DELETED": self.on_any_event,
+            "EVENT_TYPE_MODIFIED": self.on_any_event,
+            "EVENT_TYPE_MOVED": self.on_any_event,
+            "Status": self.proc_status,
+            "Cargo": self.proc_cargo,
+            # "Market": self.proc_market,
+        }
+        self.edw = EDWatcher()
+
+    def proc_market(self, log_path: Path):
+        print(f"Market Schtuff goes here")
+
+    def proc_cargo(self, log_path: Path):
+        # Update Cargo
+        self.edw.reset_cargo()
+        used_cargo = 0
+        with open(log_path) as fp:
+            lines = fp.read().replace('\n', '')
+        data = json.loads(lines)
+        for i in data['Inventory']:
+            used_cargo += i['Count']
+            self.edw.update_cargo(i)
+        self.edw.set_cargo_cur(used_cargo)
+
+        # Update contents of Cargo Hold
+
+
+    def proc_status(self, log_path: Path):
+        sf = status_enums.StatusFlag
+        with open(log_path) as fp:
+            lines = fp.readlines()
+        # There should only ever be one line in this file, so this will grab that single line.
+        data = json.loads(lines[0])
+        flags = {}
+        for flag in sf:
+            is_set = bool(flag & data["Flags"])
+            if is_set:
+                flags[flag] = {is_set}
+        self.edw.set_status_flags(flags)
+        self.edw.set_pip("sys", data["Pips"][0])
+        self.edw.set_pip("eng", data["Pips"][1])
+        self.edw.set_pip("wep", data["Pips"][2])
+
+    def on_any_event(self, event: FileModifiedEvent):
+        evt_file_name = Path(event.src_path).name
+        evt_file_stem = Path(event.src_path).stem
+        evt_file_size = os.path.getsize(event.src_path)
+        if evt_file_size == 0:
+            # print(f"{evt_file_stem} - Truncated!")
+            pass
+        else:
+            # print(f"{evt_file_stem} is now {evt_file_size} bytes. -- {evt_file_stem}")
+            if evt_file_stem in self._func_dict:
+                self._func_dict[evt_file_stem](event.src_path)
+            else:
+                print(f"No function for {evt_file_stem} yet")
+
+
+log_dir = Path("c:\\Users\\chili\\Saved Games\\Frontier Developments\\Elite Dangerous")
+# edlogwatcher = EDLogWatcher(log_dir=log_dir)
+edlogwatcher = EDLogWatcher()
+observer = Observer()
+observer.schedule(edlogwatcher, str(log_dir))
+observer.start()
--- main.py
+++ main.py
@@ -1,41 +1,20 @@
-import json
-import time
-from pathlib import Path
-# from threading import Event, Thread
-
-from watchdog.events import FileSystemEventHandler
+import sys
+import logging
 from watchdog.observers import Observer
-
-log_dir = Path('c:\\Users\\chili\\Saved Games\\Frontier Developments\\Elite Dangerous')
-
-class MyEventHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print(" - {}".format(event))
+from watchdog.events import LoggingEventHandler
 
 if __name__ == "__main__":
-    log_dir = Path('c:\\Users\\chili\\Saved Games\\Frontier Developments\\Elite Dangerous')
-    event_handler = MyEventHandler()
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = "c:\\Users\\chili\\Saved Games\\Frontier Developments\\Elite Dangerous"
+    event_handler = LoggingEventHandler()
     observer = Observer()
-    observer.schedule(event_handler, log_dir.absolute(), recursive=False)
+    observer.schedule(event_handler, path, recursive=True)
     observer.start()
-
     try:
-        while True:
-            time.sleep(1)
-    finally:
+        while observer.is_alive():
+            observer.join(1)
+    except KeyboardInterrupt:
         observer.stop()
-        observer.join()
-
-#watchme = EDLogWatcher(log_file=log_dir / 'Journal.201109145516.01.log')
-
-# watchmen = {
-#     'Cargo': EDLogWatcher(log_file=log_dir / 'Cargo.json'),
-#     'Market': EDLogWatcher(log_file=log_dir / 'Market.json'),
-#     'Modules': EDLogWatcher(log_file=log_dir / 'ModulesInfo.json'),
-#     'NavRoute': EDLogWatcher(log_file=log_dir / 'NavRoute.json'),
-#     'Outfitting': EDLogWatcher(log_file=log_dir / 'Outfitting.json'),
-#     'Shipyard': EDLogWatcher(log_file=log_dir / 'Shipyard.json'),
-#     'Status': EDLogWatcher(log_file=log_dir / 'Status.json')
-# }
-
-
+    observer.join()
